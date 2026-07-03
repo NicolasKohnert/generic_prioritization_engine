96a3f03d4695ac98cdb2cfcf7327fafadf3f25a3
NicolasKohnert <nicolas.kohnert@t-online.de>
2026-07-03 18:10:53 +0200

    Fix:NameError in error logging for invalid assets

--- engine/database.py
+++ engine/database.py
@@ -6,36 +6,31 @@ logger = logging.getLogger("Prioritization_Pipeline")
 
 def save_to_sql(assets_dicts, db_path):
     try:
-        with sqlite3.connect(db_path) as conn:
-            cursor = conn.cursor()
+        with closing(sqlite3.connect(db_path)) as conn:
+            with conn:
+                cursor = conn.cursor()
         
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS asset_history (
-                    id TEXT,
-                    score REAL,
-                    x REAL,
-                    y REAL,
-                    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-                 )
-            """)
-
-            for asset in assets_dicts:
                 cursor.execute("""
-                    INSERT INTO asset_history (id, score, x, y)
-                    VALUES (?,?,?,?)
-                """, (
-                    asset["id"],
-                    asset["score"],
-                    asset["geometry"]["x"],
-                    asset["geometry"]["y"]
-                ))
+                    CREATE TABLE IF NOT EXISTS asset_history (
+                        id TEXT,
+                        score REAL,
+                        x REAL,
+                        y REAL,
+                        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+                     )
+                """)
 
-            conn.commit()
-            logger.info(f"Successfully saved {len(assets_dicts)} records to SQLite database")
+                for asset in assets_dicts:
+                    cursor.execute("""
+                        INSERT INTO asset_history (id, score, x, y)
+                        VALUES (?,?,?,?)
+                    """, (
+                        asset["id"],
+                        asset["score"],
+                        asset["geometry"]["x"],
+                        asset["geometry"]["y"]
+                    ))
+
+        logger.info(f"Successfully saved {len(assets_dicts)} records to SQLite database")
     except sqlite3.Error as e:
         logger.error(f"Database error: {e}")
-
-    finally:
-        if conn:
-            conn.close()
-            logging.debug("Database connection closed.")
